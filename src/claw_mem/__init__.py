03b7d58b483d9513dbda4a8075d60df652ed7065
Peter Cheng <380329+petercheng@users.noreply.github.com>
2026-05-18 17:59:07 +0800

    chore: sync version to 3.0.0-rc.2

--- src/claw_mem/__init__.py
+++ src/claw_mem/__init__.py
@@ -29,7 +29,7 @@ v0.9.0 Features (2026-03-22):
 - 100% English documentation
 """
 
-__version__ = "3.0.0rc1"
+__version__ = "3.0.0rc2"
 __author__ = "Peter Cheng"
 
 from .memory_manager import MemoryManager
